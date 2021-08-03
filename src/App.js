ab47e403bd93e4bcb5ccc6ecace4e8ca2fb5c97f
davide <davide.lista@mail.polimi.it>
2021-08-03 19:56:33 +0200

    Beta 2.2 - davidelista.com - React js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Home, Details} from "./pages";
 import {Footer, Navbar} from "./components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Redirect } from "react-router";
 import { AppContext, useAppContext } from "./context/AppContext";
 import ScrollToTop from "./pages/ScrollToTop";
+import ReactGA from "react-ga";
 
 const App = () => {
+
+    useEffect(() => {
+        ReactGA.initialize("UA-142377641-1 ");
+        //to report page view
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
 
     const appData = useAppContext();
 
